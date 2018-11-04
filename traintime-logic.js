2ea67c6f5741ed08ef01baae350a4b4304ee00de
Riley Griffin <rigriffin241@gmail.com>
2018-11-04 15:40:12 -0500

    first commit

---
+++ traintime-logic.js
@@ -0,0 +1,12 @@
+// Steps to complete:
+
+// 1. Create Firebase link
+// 2. Create initial train data in database
+// 3. Create button for adding new trains - then update the html + update the database
+// 4. Create a way to retrieve trains from the trainlist.
+// 5. Create a way to calculate the time way. Using difference between start and current time.
+//    Then take the difference and modulus by frequency. (This step can be completed in either 3 or 4)
+
+const config = {
+
+}
